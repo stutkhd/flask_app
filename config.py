3757b8880f7bda058b90dee88b123ff91b9f9ad7
stutkhd <you@example.com>
2019-12-21 01:42:51 +0900

    Add config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+#自分のtwitterAPItoken
+CONFIG = {
+   "CONSUMER_KEY":"xxxxxxxxxxxxxxxxxxxxxxxxxx",
+   "CONSUMER_SECRET":"xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+   "ACCESS_TOKEN":"xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+   "ACCESS_TOKEN_SECRET":"xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+   }
